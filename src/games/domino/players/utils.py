124b348aef9cbf04566a17dbfb0d0ee1388714dd
Lázaro Raúl <riglesiasvera@gmail.com>
2021-08-24 23:06:53 -0400

    Collect the data from history
    Method: game_data_collector

--- src/games/domino/players/utils.py
+++ src/games/domino/players/utils.py
@@ -5,4 +5,24 @@ def count_min(player, piece):
         cant[1] += (piece[1] in item)
     val = min(cant)
     return val, cant.index(val)
+
     
+def game_data_collector(current_hand, player_id, history):
+    pieces = [[], [], [], []]
+    pieces[player_id].extend(current_hand)
+    missing = [[], [], [], []]
+
+    heads = [-1, -1]
+    empty = [-1, -1]
+    for event, *data in history:
+        if event.name == 'MOVE':
+            move, id, head = data 
+            pieces[id].append(move)
+            if heads == empty:
+                heads = move
+            else:
+                heads[head] = move[move[0] == heads[head]]
+        elif event.name == 'PASS':
+            id = data[0]
+            missing[id].extend(heads)
+    return pieces, missing  
