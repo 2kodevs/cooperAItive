3be2ce4704ef9975359c2174b2dee4c5a8e2ee86
Miguel Tenorio <mtpotro41@gmail.com>
2021-09-02 16:32:47 -0400

    Upgrade AZero trainer
    New features:
    - Support to save and load training data
    - If loaded data is insufficient, then generate data via self play
    - Add parallel support for self play

--- src/training/alpha_zero_trainer.py
+++ src/training/alpha_zero_trainer.py
@@ -1,8 +1,13 @@
+from multiprocessing import Pool
 from ..models import alpha_zero_net as Net
 from .trainer import Trainer
 
 import numpy as np
 import random
+import time
+import os
+import json
+
 class AlphaZeroTrainer(Trainer):
     """
     Trainer manager for Alpha Zero model
@@ -32,12 +37,67 @@ class AlphaZeroTrainer(Trainer):
         # The NN is in self.net, you can pass it to your methods.
         pass
 
-    def policy_iteration(self, epoch, rollouts, verbose=False):
-        data = []
-        while len(data) < self.batch_size:
-            data.extend(self.self_play(rollouts))
-            
+    def policy_iteration(self, epoch: int, simulate: bool, num_process=1, verbose=False, save_data=False):
+        data = self.get_data(simulate, num_process, verbose, self.batch_size)
+        
+        if save_data:
+            num_files = len([name for name in os.listdir(self.data_path) if os.path.isfile(name)])
+            path = f'{self.data_path}/training_data_{num_files}'
+            with open(path, 'w') as writer:
+                json.dump(data, writer)
+
         batch = random.sample(data, self.batch_size)
         loss = self.net.train(batch)
         Trainer.adjust_learning_rate(epoch, self.net.optimizer)
         return loss
+
+    def get_data(self, simulate: bool, num_process: int, verbose: bool, batch_size: int):
+        data = []
+        num_games = 0
+
+        if simulate:
+            if verbose:
+                print('Simulations started')
+                start = time.time()
+
+            if num_process > 1:
+                while len(data) < batch_size:
+                    jobs = [self.rollouts] * (batch_size / num_process)
+                    num_games += batch_size / num_process
+                    pool = Pool(num_process)
+                    new_data = pool.map(self.self_play, jobs)
+                    pool.close()
+                    pool.join()
+                    data.extend(new_data)
+            else:
+                while len(data) < batch_size:
+                    data.extend(self.self_play(self.rollouts))
+                    num_games += 1
+
+            if verbose:
+                print(f'Simulated {num_games} in {str(int(time.time() - start))} seconds')
+        else:
+            # Get saved training data
+            if verbose:
+                print('Loading saved data...')
+            file_names = [name for name in os.listdir(self.data_path) if os.path.isfile(name)]
+            file_names.sort()
+            file_names.reverse() # Reverse to get newest data first
+
+            for name in file_names:
+                path = f'{self.data_path}/{name}'
+
+                with open(path, 'r') as reader:
+                    data.extend(json.load(reader))
+
+                if verbose:
+                    print(f'File {name} loaded. Current batch_size: {len(data)}')
+
+                if len(data) >= batch_size:
+                    break
+            if len(data) < batch_size:
+                if verbose:
+                    print('Insuficient data. Proceding to generate data with self play')
+                data.extend(self.get_data(True, num_process, verbose, batch_size - len(data)))
+
+        return data
