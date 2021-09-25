c60286fc4587e24fdbcd78eb0cf5de660acaf49d
Lázaro Raúl <riglesiasvera@gmail.com>
2021-09-25 18:31:09 -0400

    fix: Hand numbers should go from 1 to 12

--- src/games/sequence/module/hands/hand_out.py
+++ src/games/sequence/module/hands/hand_out.py
@@ -3,14 +3,14 @@ from ..defaults import Card
 from .utils import take
 from random import shuffle
 
-def hand_out(number_of_player, pieces_per_player):
+def handout(number_of_player, pieces_per_player):
     # Check valid game distribution
     assert 4 * 12 >= number_of_player * pieces_per_player, "Not enough cards for this game"
 
     # Generate the cards
     cards = []
     for ctype in range(4):
-        for num in range(13):
+        for num in range(1, 13):
             cards.append((Card(ctype), num))
     shuffle(cards)
 
