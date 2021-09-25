735739ef6a37aa6b96aa5bfbe8f012a3becc3879
Lázaro Raúl <riglesiasvera@gmail.com>
2021-09-25 16:43:07 -0400

    feat: Basic Handout

---
+++ src/games/sequence/module/hands/hand_out.py
@@ -0,0 +1,20 @@
+from ..players import PlayerView
+from ..defaults import Card
+from .utils import take
+from random import shuffle
+
+def hand_out(number_of_player, pieces_per_player):
+    # Check valid game distribution
+    assert 4 * 12 >= number_of_player * pieces_per_player, "Not enough cards for this game"
+
+    # Generate the cards
+    cards = []
+    for ctype in range(4):
+        for num in range(13):
+            cards.append((Card(ctype), num))
+    shuffle(cards)
+
+    # handout the player cards and the deck 
+    iterator = iter(cards)
+    hands = [PlayerView(list(take(iterator, pieces_per_player))) for _ in range(number_of_player)]
+    return hands, list(iterator)
