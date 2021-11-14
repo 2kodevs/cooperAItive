749377d5d835ac812d95827713b61b84793cab86
Lázaro Raúl <riglesiasvera@gmail.com>
2021-11-14 18:50:11 -0500

    feat: Heuristic player

--- src/games/domino/module/players/strategies/__init__.py
+++ src/games/domino/module/players/strategies/__init__.py
@@ -21,6 +21,16 @@ from .human import Human
 from .utils import alphazero as alphazero_utils, mc as mc_utils, game as game_utils
 from .models import alpha_zero_model as AlphaZeroModel, AlphaZeroNet
 
+
+class Shortcut:
+    def __init__(self, name, cls) -> None:
+        self.__name__ = name
+        self.cls = cls
+
+    def __call__(self, *args, **kwds):
+        return self.cls(*args, **kwds)
+
+
 # Add players to this list
 PLAYERS = [
     BigDrop,
@@ -43,4 +53,11 @@ PLAYERS = [
     Human,
     MonteCarlo,
     AlphaZero,
+    Shortcut("MC", MonteCarlo),
+    Shortcut("A0", AlphaZero),
+    Shortcut("heuristic", MergeFactory([
+        SmallDrop, AlwaysDouble, Agachao, DataKeeper,
+        DataDropper, SimpleHybrid, TableCounter, LessPlayed,
+        Passer, Repeater, Frequent, BigDrop,
+    ])),
 ]
