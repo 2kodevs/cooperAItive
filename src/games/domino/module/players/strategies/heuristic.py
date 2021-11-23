14dc5f868839001d124f86bd16fed07184a30f3c
Miguel Tenorio <mtpotro41@gmail.com>
2021-11-23 10:44:24 -0500

    feat: Update heuristic player for Domino

--- src/games/domino/module/players/strategies/heuristic.py
+++ src/games/domino/module/players/strategies/heuristic.py
@@ -10,8 +10,8 @@ from .passer import Passer
 import numpy as np
 
 
-strategies        = [SimpleHybrid, BigDrop, DataDropper, Agachao, Repeater, AlwaysDouble, TableCounter, Frequent, Passer]
-acummulated_value = [        0.26,     0.2,        0.16,    0.13,     0.09,         0.07,         0.05,     0.03,   0.01]
+strategies        = [BigDrop, DataDropper, Agachao, Repeater, AlwaysDouble, TableCounter, Frequent, Passer]
+acummulated_value = [0.248,   0.213,       0.178,   0.142,    0.105,        0.07,         0.034,    0.01]
 
 
 class Heuristic(*strategies):
@@ -20,5 +20,5 @@ class Heuristic(*strategies):
         BasePlayer.__init__(self, f'Heuristic::{name}')
 
     def filter(self, valids):
-        x = np.random.choice(list(range(9)), p=acummulated_value)
+        x = np.random.choice(list(range(8)), p=acummulated_value)
         return strategies[x].filter(self, valids)
